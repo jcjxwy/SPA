91b1f14a7e5ba0d8b11b2b852e803b3eb55927cc
Khor Shao Liang <stephenksl@gmail.com>
2019-08-23 22:39:52 +0800

    Pkb variable table (#2)
    * PKB.h: update VarTable API
    
    * PKB.cpp: implement VarTable API
    
    * Create .gitignore
    
    * Fix bugs

--- Code10/source/PKB.h
+++ Code10/source/PKB.h
@@ -4,9 +4,11 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_map> 
 
 using namespace std;
 typedef short PROC;
+typedef int INDEX;
 
 class TNode;
 
@@ -14,8 +16,13 @@ class VarTable;  // no need to #include "VarTable.h" as all I need is pointer
 
 class PKB {
 public:
-	static VarTable* varTable; 
+	static VarTable* varTable;
 	static int setProcToAST(PROC p, TNode* r);
-	static TNode* getRootAST (PROC p);
+	static TNode* getRootAST(PROC p);
 
+	INDEX insertVariable(std::string variableName);
+	std::string getVariableName(INDEX index);
+	INDEX getVariableIndex(std::string variableName);
+	int getSize();
+	std::unordered_map<std::string, INDEX> PKB::getVariables();
 };
